c1f8f95473a1c02afdb48f1c374b1172de819bc4
Akeishamontero <paemontero@brc.pshs.edu.ph>
2026-09-25 21:54:37 +0800

    Add APPS class with methods for app management

--- q1/advancedRelationships.py
+++ q1/advancedRelationships.py
@@ -15,3 +15,38 @@ class CATALOG:
         self.count_items += 1
         print("Catalog updated.")
         print("New Count of Number of Items in Catalog:", self.count_items)
+
+class APPS:
+
+    def __init__(self, name, developer, price, available, settings, profile):
+       self.name = name
+       self.developer = developer
+       self.__settings = settings
+       self.available = available
+       self.profile = profile
+       self.price = price
+
+    def Launch(self):
+       print(self.name, "has been launched.")
+
+    def Updateversion(self, versionNumber, string):
+       self.__settings = string
+       print(self.name, "has been updated to version", versionNumber)
+
+    def Download(self):
+        if self.available:
+           print(self.name, "is downloading.")
+        else:
+           print(self.name, "is not available for download.")
+
+    def get_settings(self):
+        return self.__settings
+
+
+    def display_info(self):
+        print("App Name:", self.name)
+        print("Developer:", self.developer)
+        print("Price:", self.price)
+        print("Available:", self.available)
+        print("Profile:", self.profile)
+
