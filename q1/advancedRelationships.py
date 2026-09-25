ff5d37ecac0e445e2326eb1b11010e252e7b7e81
Akeishamontero <paemontero@brc.pshs.edu.ph>
2026-09-25 22:36:16 +0800

    Add new entertainment objects to advancedRelationships.py

--- q1/advancedRelationships.py
+++ q1/advancedRelationships.py
@@ -86,5 +86,6 @@ entertainment_object1 = ENTERTAINMENT_APP("Among Us", "Innersloth", 4.99, True, 
 entertainment_object2 = ENTERTAINMENT_APP("Minecraft", "Mojang", 6.99, True, "Dark Mode", "Gamergirl123", "Sandbox", "Video Game", 4.8)
 entertainment_object3 = ENTERTAINMENT_APP("TikTok", "Zhang Yiming", 0.00, True, "Dark Mode", "Starrynights11", "Social Entertainment", "Short Videos", 4.3)
 
+
          
 
