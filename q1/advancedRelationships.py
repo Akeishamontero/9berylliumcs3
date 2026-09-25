53f90b0470b0668f3168ac9bae271c473b320fa8
Akeishamontero <paemontero@brc.pshs.edu.ph>
2026-09-25 14:29:04 +0800

    Add CATALOG class with initialization parameters

---
+++ q1/advancedRelationships.py
@@ -0,0 +1,6 @@
+class CATALOG:
+    def __init__(self, catalog_name: str, category: str, access_level: str, count_items: int):
+        self.catalog_name = catalog_name
+        self.category = category
+        self.access_level = access_level
+        self.count_items = count_items
