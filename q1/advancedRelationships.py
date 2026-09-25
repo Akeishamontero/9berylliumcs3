0d1ce6cc37bc38a8df0848a5b2e1867347b2a36b
Akeishamontero <paemontero@brc.pshs.edu.ph>
2026-09-25 14:37:42 +0800

    Add display and update methods to Catalog class
    Added methods to display and update catalog information.

--- q1/advancedRelationships.py
+++ q1/advancedRelationships.py
@@ -4,3 +4,14 @@ class CATALOG:
         self.category = category
         self.access_level = access_level
         self.count_items = count_items
+
+    def display_catalog(self): 
+        print("Catalog Name:", self.catalog_name)
+        print("Category:", self.category)
+        print("Access Level:", self.access_level)
+        print("Number of Items in Catalog:", self.count_items)
+
+    def update_catalog(self): 
+        self.count_items += 1
+        print("Catalog updated.")
+        print("New Count of Number of Items in Catalog:", self.count_items)
